8e343570e90470dc9f46e08b29a8ae02bd7092c0
DucThanh <phoducthanh@gmail.com>
2024-11-06 23:48:56 +0700

    Add project files.

---
+++ Services/Shared/HMS.Shared.Constant/Database/DbSchema.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Shared.Constant.Database
+{
+    public class DbSchema
+    {
+        public const string TableMigrationsHistory = "__EFMigrationsHistory";
+        public const string Default = "dbo";
+        public const string Auth = "auth";
+        public const string Hotel = "hol";
+        public const string Room = "room";
+        public const string BillBooking = "bill";
+        public const string Promotion = "promo";
+        public const string Notification = "noti";
+    }
+}
