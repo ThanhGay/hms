21e9b42bf330916b62e49559b5a6cfab5511fd7a
DucThanh <phoducthanh@gmail.com>
2024-11-06 23:52:19 +0700

    Add project files.

---
+++ HMS.WebAPI/Controllers/User/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HMS.WebAPI.Controllers.User
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
