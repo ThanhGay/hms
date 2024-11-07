1a554d73b9ec1f1bd4bb1495499fb9fe9ce63f30
DucThanh <phoducthanh@gmail.com>
2024-11-07 08:36:42 +0700

    Remove Room, Promotion Domain

--- Services/Shared/HMS.Shared.Constant/Database/DbSchema.cs
+++ Services/Shared/HMS.Shared.Constant/Database/DbSchema.cs
@@ -12,9 +12,7 @@ namespace HMS.Shared.Constant.Database
         public const string Default = "dbo";
         public const string Auth = "auth";
         public const string Hotel = "hol";
-        public const string Room = "room";
         public const string BillBooking = "bill";
-        public const string Promotion = "promo";
         public const string Notification = "noti";
     }
 }
--- Services/Shared/HMS.Shared.Constant/Permission/PermissionKeys.cs
+++ Services/Shared/HMS.Shared.Constant/Permission/PermissionKeys.cs
@@ -8,5 +8,8 @@ namespace HMS.Shared.Constant.Permission
 {
     public class PermissionKeys
     {
+        public const string AddRoom = "create_room_in_hotel";
+        public const string UpdateRoom = "change_information_of_room";
+        public const string ChangePriceRoom = "change_price_of_room";
     }
 }
