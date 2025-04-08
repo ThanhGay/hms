07b8f94b2cea32dc508804ce50bc332c0059421e
DucThanh <phoducthanh@gmail.com>
2025-04-08 13:54:36 +0700

    not

--- Services/Auth/HMS.Auth.Infrastructures/AuthDbContext.cs
+++ Services/Auth/HMS.Auth.Infrastructures/AuthDbContext.cs
@@ -75,9 +75,9 @@ namespace HMS.Auth.Infrastructures
                 .HasForeignKey(f => f.CustomerId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            SeedData.SeedAuthRole(modelBuilder);
-            SeedData.SeedAuthRolePermission(modelBuilder);
-            SeedData.SeedAccount(modelBuilder);
+            //SeedData.SeedAuthRole(modelBuilder);
+            //SeedData.SeedAuthRolePermission(modelBuilder);
+            //SeedData.SeedAccount(modelBuilder);
 
             base.OnModelCreating(modelBuilder);
 
